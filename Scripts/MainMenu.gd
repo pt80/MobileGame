a7fbe475d2c339e70150f877c138e56cfb3bc172
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-01 14:12:26 -0500

    file setup

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('dakotas level loaded')
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/DakotaLevel.tscn")
