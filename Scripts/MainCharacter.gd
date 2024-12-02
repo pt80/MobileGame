076981d0e8a0f0243f704aadfce357f33aa4c460
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-02 10:51:36 -0500

    top down movement added

---
+++ Scripts/MainCharacter.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(_delta):
+	var VerticalDirection = Input.get_axis("up", "down")
+	if VerticalDirection:
+		velocity.y = VerticalDirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	var HorizontalDirection = Input.get_axis("left", "right")
+	if HorizontalDirection:
+		velocity.x = HorizontalDirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
