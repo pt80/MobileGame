c54185ffae55584c61dd5d0c5bba847f7b45c329
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-02 18:52:43 -0500

    idk

--- Scripts/EndPoint.gd
+++ Scripts/EndPoint.gd
@@ -5,12 +5,6 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body):
 	if body.name == "MainCharacter":
 		print('level complete')
