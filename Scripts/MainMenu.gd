f2f7e41b537934a7e584c5fb8ef53c719be6538f
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-03 09:09:52 -0500

    get level folder

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,13 +3,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print('dakotas level loaded')
+	GetLevels("res://Scenes/Levels/")
 
+func GetLevels(path):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/DakotaLevel.tscn")
