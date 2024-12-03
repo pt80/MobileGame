cffa116800b8a7086e2fe49d6502c8fb6e3c9d86
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-03 12:43:21 -0500

    Update LevelInfo.gd

--- Scripts/LevelInfo.gd
+++ Scripts/LevelInfo.gd
@@ -2,10 +2,12 @@ extends Node
 
 var Metadata = {
 	"DakotaLevel":{
+		'scene': "res://Scenes/Levels/DakotaLevel.tscn",
 		'image': "res://Images/LevelImage.png",
 		'high score': 0
 	},
 	"NeilLevel":{
+		'scene': "res://Scenes/Levels/NeilLevel.tscn",
 		'image': "res://Images/MainMenuBackground.png",
 		'high score': 0
 	}
