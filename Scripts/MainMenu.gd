3ae267e4e67352d7d85a18d49f749057d34e9b73
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-03 12:40:34 -0500

    update level text when switched

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,10 +1,14 @@
 extends Control
 
+@onready var level_image = $VBoxContainer/HBoxContainer2/LevelImage
+@onready var level_name_label = $VBoxContainer/HBoxContainer/LevelNameLabel
+
 var LevelIndex = 0
 var LevelList: Array = []
 
 func _ready():
 	GetLevels("res://Scenes/Levels/")
+	SetLevelInfo()
 
 func GetLevels(path):
 	var dir = DirAccess.open(path)
@@ -18,9 +22,6 @@ func GetLevels(path):
 	else:
 		print("An error occurred when trying to access the path.")
 	print(LevelList)
-	
-func _process(_delta):
-	pass
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/" + LevelList[LevelIndex])
@@ -29,10 +30,16 @@ func _on_previous_button_pressed():
 	LevelIndex -= 1
 	if LevelIndex < 0:
 		LevelIndex = LevelList.size() - 1
-	print('show previous level', LevelIndex)
-
+	SetLevelInfo()
+	
 func _on_next_button_pressed():
 	LevelIndex += 1
 	if LevelIndex > LevelList.size() - 1: 
 		LevelIndex = 0
-	print('show next level', LevelIndex)
+	SetLevelInfo()
+
+func SetLevelInfo():
+	var LevelName =LevelList[LevelIndex].left(LevelList[LevelIndex].length() - 5)
+	level_image.texture = load(LevelInfo.Metadata[LevelName]['image'])
+	level_name_label.text = LevelName
+
