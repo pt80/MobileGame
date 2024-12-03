deff3c086c5c00b05176dead992175b33105ee77
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-03 12:22:44 -0500

    level select added

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,7 +1,8 @@
 extends Control
 
+var LevelIndex = 0
+var LevelList: Array = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	GetLevels("res://Scenes/Levels/")
 
@@ -12,18 +13,26 @@ func GetLevels(path):
 		var file_name = dir.get_next()
 		while file_name != "":
 			print("Found file: " + file_name)
+			LevelList.append(file_name)
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
-
+	print(LevelList)
+	
 func _process(_delta):
 	pass
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/DakotaLevel.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/" + LevelList[LevelIndex])
 
 func _on_previous_button_pressed():
-	pass # Replace with function body.
+	LevelIndex -= 1
+	if LevelIndex < 0:
+		LevelIndex = LevelList.size() - 1
+	print('show previous level', LevelIndex)
 
 func _on_next_button_pressed():
-	pass # Replace with function body.
+	LevelIndex += 1
+	if LevelIndex > LevelList.size() - 1: 
+		LevelIndex = 0
+	print('show next level', LevelIndex)
