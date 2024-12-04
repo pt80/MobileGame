8bea2ce94b6006284a1611ba8944cb6c14f4b164
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-04 09:13:18 -0500

    added enemy movement

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,11 +1,20 @@
-extends Area2D
+extends CharacterBody2D
 
+const Speed = 200
+var Direction = 1
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func _physics_process(delta):
+	if ray_cast_left.is_colliding() or ray_cast_right.is_colliding():
+		Direction *= -1
+	velocity.x =  Direction * Speed
+	move_and_slide()
+	
 func _on_body_entered(body):
 	if body.name == "MainCharacter":
 		body.Death()
+
+
+
 
