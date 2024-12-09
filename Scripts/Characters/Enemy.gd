42654b3b1439b386e24534da9db1e5ecf5704153
pt80 <45863938+pt80@users.noreply.github.com>
2024-12-09 16:22:00 -0500

    direction added

--- Scripts/Characters/Enemy.gd
+++ Scripts/Characters/Enemy.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 const Speed = 200
-var Direction = 1
+@export var VelocityDirection: int = 1
 
 func _physics_process(_delta):
-	velocity.x =  Direction * Speed
+	velocity.x =  VelocityDirection * Speed
 	move_and_slide()
 
 func _on_area_2d_body_entered(body):
 	if body.name == "MainCharacter":
 		body.Death()
 	else:
-		Direction *= -1
+		VelocityDirection *= -1
